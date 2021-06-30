67c4cbfdbdb4ff0d4c69e9a05e1754ec7427cc7a
monika <monika@orl25.de>
2021-06-30 14:01:47 +0200

    added some more colors and expanded the scalars preprocessing function so that it can now also output capacities for energy flows (but not for storage)

--- analysis/plot.py
+++ analysis/plot.py
@@ -7,15 +7,22 @@ colors = {'Solar_PV': '#bcbd22', 'Wind': '#407294', 'Wind_Offshore': '#1f77b4', 
           'SecondaryEnergy_Electricity_Slack': '#bf3636',
           'SecondaryEnergy_Heat_Slack': '#bf3636',
           'SecondaryEnergy_Heat_ElectricityHeat_Large': '#3078a3',
+          'Capacity_Heat_ElectricityHeat_Large': '#3078a3',
+          'Capacity_Heat_ElectricityHeat_Small': '#6fbeee',
           'SecondaryEnergy_Heat_ElectricityHeat_Small': '#6fbeee',
+
           'SecondaryEnergy_Heat_Electricity_Large': '#36b07b',
+          'Capacity_Heat_Electricity_Large': '#36b07b',
           'Hydro': '#bfd1ce',
           'H2CavernStorage': '#54f2ff',
           'Biomass': '#2ca02c', 'CH4': '#d62728',
+          'Capacity_Heat_CH4_Large': '#d46566',
           'SecondaryEnergy_Electricity_CH4_GT': '#6d562b', 'CH4_GT': '#6d562b',
           'SecondaryEnergy_Heat_Gas_Large': '#b07911',
           'SecondaryEnergy_Electricity_ElectricityHeat_CH4_ExCCGT': '#a81bc3',
           'SecondaryEnergy_Heat_ElectricityHeat_CH4_ExCCGT': '#ca71db',
+          'Capacity_ElectricityHeat_CH4_ExCCGT': '#e0a2ec',
+
           'Nuclear': '#e4f200',
           'Combustible Fuels': '#d62728', 'BAT discharge': '#9467bd',
           'Import': '#17becf', 'Shortage': '#ff7f0e', 'BAT charge': '#e377c2', 'Export': '#8c564b',
@@ -23,7 +30,9 @@ colors = {'Solar_PV': '#bcbd22', 'Wind': '#407294', 'Wind_Offshore': '#1f77b4', 
           'Demand': '#000000',
           'Solar Thermal': '#ecd70e', 'Geothermal': '#cdb79e', 'Tide, Wave and Ocean': '#133337',
           'Other Sources': '#e0d6ce',
-          'LiIonBatteryCharge': '#ff80ed', 'LiIonBatteryDischarge': '#ffc0cb', 'LiIonBatteryStorage': '#cdb79e',
+          'LiIonBatteryCharge': '#ff80ed', 'LiIonBatteryDischarge': '#ffc0cb',
+          'Storage_Capacity_Electricity_LiIonBatteryStorage': '#cdb79e',
+          'LiIonBatteryStorage': '#cdb79e',
           'Storage_Input_Electricity_LiIonBatteryStorage': '#a4a1e3',
           'Storage_Losses_Electricity_LiIonBatteryStorage': '#694872',
           'Storage_Output_Electricity_LiIonBatteryStorage': '#c321ee',
@@ -35,6 +44,7 @@ colors = {'Solar_PV': '#bcbd22', 'Wind': '#407294', 'Wind_Offshore': '#1f77b4', 
           'Storage_Losses_Heat_LargeStorage': '#858052',
           'Storage_Output_Heat_SmallStorage': '#e5a305',
           'Storage_Output_Heat_LargeStorage': '#f4df0b',
+          'Storage_Capacity_Electricity_H2CavernStorage': '#1bbf9a',
           'Energy_FinalEnergy_Electricity': '#000000',
           'Energy_FinalEnergy_Electricity_H2': '#5f7c83',
           'Energy_FinalEnergy_Heat_CHP': '#292585',
@@ -66,7 +76,11 @@ def preprocessing_stacked_scalars(plot_data, factor, onxaxes): # put a factor he
     df_plot_conversion_electricity = pd.DataFrame()
     df_plot_storage_heat = pd.DataFrame()
     df_plot_storage_electricity = pd.DataFrame()
+    df_plot_capacity_heat = pd.DataFrame()
+    df_plot_capacity_electricity = pd.DataFrame()
 
+    import pdb
+    pdb.set_trace()
     if plot_data['Parameter'].str.contains('EnergyConversion_Capacity_Electricity').any():
         plot_data['Parameter'] = plot_data['Parameter'].str.replace('EnergyConversion_Capacity_Electricity_', '')
     if plot_data['Parameter'].str.contains('EnergyConversion_').any():
@@ -74,7 +88,9 @@ def preprocessing_stacked_scalars(plot_data, factor, onxaxes): # put a factor he
     if plot_data['Parameter'].str.contains('Storage').any():
         # store storage in a separate dataframe ...
         df_storage = plot_data.loc[plot_data['Parameter'].str.contains('Storage'), :]
-        df_storage['Parameter'] = df_storage['Parameter'].str.replace('Storage_Capacity_Electricity_', '')
+        # df_storage['Parameter'] = df_storage['Parameter'].str.replace('Storage_Capacity_Electricity_', '')
+        # Show only output and losses and not inputs; capacities should be plotted separately
+        df_storage = df_storage.loc[~df_storage['Parameter'].str.contains('Input|Capacity'), :]
         # ... and remove it from the original one
         plot_data = plot_data[~plot_data['Parameter'].str.contains('Storage')]
         if df_storage['Parameter'].str.contains('Heat').any():
@@ -93,6 +109,13 @@ def preprocessing_stacked_scalars(plot_data, factor, onxaxes): # put a factor he
         df_plot_conversion_heat = pd.crosstab(index=df_conversion_heat[onxaxes], columns=df_conversion_heat.Parameter,
                                               values=df_conversion_heat.Value / factor, aggfunc='mean')
     df_conversion_electricity = plot_data[~plot_data['Parameter'].str.contains('SecondaryEnergy_Heat|Energy_FinalEnergy_Heat')]
+    if plot_data['Parameter'].str.contains('Capacity_Heat').any():
+        df_capacity_heat = plot_data.loc[plot_data['Parameter'].str.contains('Capacity_Heat')]
+        df_plot_capacity_heat = pd.crosstab(index=df_capacity_heat[onxaxes], columns=df_capacity_heat.Parameter,
+                                            values=df_capacity_heat.Value / factor, aggfunc='mean')
+        df_capacity_electricity = plot_data.loc[~plot_data['Parameter'].str.contains('Capacity_Heat')]
+        df_plot_capacity_electricity = pd.crosstab(index=df_capacity_electricity[onxaxes], columns=df_capacity_electricity.Parameter,
+                                            values=df_capacity_electricity.Value / factor, aggfunc='mean')
 
 #        df_plot_conversion_heat = pd.crosstab(index=df_conversion_heat.UseCase, columns=df_conversion_heat.Parameter,
 #                                       values=df_conversion_heat.Value / factor, aggfunc='mean')
@@ -114,7 +137,8 @@ def preprocessing_stacked_scalars(plot_data, factor, onxaxes): # put a factor he
     # Storage should be plotted in a separate diagram. It is therefore herewith allocated to a separate DataFrame
     # and removed from df_plot.
 
-    return df_plot_conversion_heat, df_plot_conversion_electricity, df_plot_storage_heat, df_plot_storage_electricity
+    return df_plot_conversion_heat, df_plot_conversion_electricity, df_plot_storage_heat, df_plot_storage_electricity, \
+           df_plot_capacity_electricity, df_plot_capacity_heat
 
 
 
@@ -122,11 +146,8 @@ def stacked_scalars(df_plot, title, ylabel, xlabel):
     if df_plot.empty:
         pass
     else:
-
         # Take care that Energy demand is always in the first row. It would have probably been easier to just add the
         # numbers without appending them to the dataframe.
-        import pdb
-        pdb.set_trace()
         total_demand = 0
         if df_plot.columns.str.contains('Energy_FinalEnergy').any():
             total_demand = df_plot.iloc[0,:].sum()
