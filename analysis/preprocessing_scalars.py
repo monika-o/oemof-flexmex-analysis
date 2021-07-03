d5f353f80e83967c244086d681f3e46becda7e8d
monika <monika@orl25.de>
2021-07-03 16:55:27 +0200

    added function for preprocessing electricity flows in FlexMex2_2

--- analysis/preprocessing_scalars.py
+++ analysis/preprocessing_scalars.py
@@ -1,51 +1,66 @@
 import pandas as pd
 
-def conversion_electricity_FlexMex2_1(plot_data, onxaxes):
+def onxaxes_preparation(plot_data, onxaxes, scenario_regions):
     if onxaxes == 'Region':
-        plot_data = plot_data.loc[plot_data['Scenario'] == 'FlexMex2_1a', :]
+        plot_data = plot_data.loc[plot_data['Scenario'] == scenario_regions, :]
     elif onxaxes == 'Scenario':
 
         plot_data = plot_data.loc[plot_data['Region'].str.contains('DE'), :]
     else:
         print("Only Region or Scenario can be on the x axes.")
+
+    return plot_data
+
+def sum_transmissions(plot_data, scenario,  region):
+    df_total_outgoing = plot_data[(plot_data.loc[:, 'Parameter'] == 'Transmission_Flows_Electricity_Grid') &
+                                  (plot_data.loc[:, 'Scenario'] == scenario) &
+                                  (plot_data.loc[:, 'Region'].str.contains('_'+region))]
+
+    total_outgoing = -df_total_outgoing['Value'].sum()
+    row_total_outgoing = {'Scenario': scenario, 'Region': region, 'Parameter': 'Transmission_Outgoing', 'Unit': 'GWh',
+                          'Value': total_outgoing}
+    df_total_incoming = plot_data[(plot_data.loc[:, 'Parameter'] == 'Transmission_Flows_Electricity_Grid') &
+                                  (plot_data.loc[:, 'Scenario'] == scenario) &
+                                  (plot_data.loc[:, 'Region'].str.contains(region+'_'))]
+    total_incoming = df_total_incoming['Value'].sum()
+    row_total_ingoing = {'Scenario': scenario, 'Region': region, 'Parameter': 'Transmission_Incoming', 'Unit': 'GWh',
+                         'Value': total_incoming}
+    df_total_losses = plot_data[(plot_data.loc[:, 'Parameter'] == 'Transmission_Losses_Electricity_Grid') &
+                                (plot_data.loc[:, 'Scenario'] == scenario) &
+                                (plot_data.loc[:, 'Region'].str.contains(region))]
+    total_losses = -df_total_losses['Value'].sum()
+    row_total_losses = {'Scenario': scenario, 'Region': region, 'Parameter': 'Transmission_Losses', 'Unit': 'GWh',
+                        'Value': total_losses}
+
+    plot_data.drop(df_total_outgoing.index.to_list(), inplace=True)
+    plot_data.drop(df_total_incoming.index.to_list(), inplace=True)
+    plot_data.drop(df_total_losses.index.to_list(), inplace=True)
+    plot_data = plot_data.append(row_total_outgoing, ignore_index=True)
+    plot_data = plot_data.append(row_total_ingoing, ignore_index=True)
+    plot_data = plot_data.append(row_total_losses, ignore_index=True)
+
+    return plot_data
+
+def conversion_electricity_FlexMex2_1(plot_data, onxaxes):
+    plot_data = onxaxes_preparation(plot_data, onxaxes, 'FlexMex2_1c')
     plot_data.to_csv('2021-07-03_plot_data.csv')
 
     parameters = ['EnergyConversion_SecondaryEnergy_Electricity_CH4_GT',
                   'EnergyConversion_SecondaryEnergy_Electricity_RE',
                   'EnergyConversion_SecondaryEnergy_Electricity_Slack',
-                  'EnergyConversion_Curtailment_Electricity_RE',
                   'Transmission_Flows_Electricity_Grid',
-                  'Transmission_Losses_Electricity_Grid']
+                  'Transmission_Losses_Electricity_Grid',
+                  'EnergyConversion_Curtailment_Electricity_RE']
     plot_data = plot_data.loc[plot_data['Parameter'].isin(parameters)]
     # sum all outgoing and all ingoing transmissions for each scenario
     if onxaxes == 'Scenario':
-        for i in ('FlexMex2_1a', 'FlexMex2_1b', 'FlexMex2_1c', 'FlexMex2_1d'):
-
-            df_total_outgoing = plot_data[(plot_data.loc[:, 'Parameter'] =='Transmission_Flows_Electricity_Grid') &
-                                          (plot_data.loc[:, 'Scenario'] == i) &
-                                          (plot_data.loc[:, 'Region'].str.contains('_DE'))]
-
-            total_outgoing = -df_total_outgoing['Value'].sum()
-            row_total_outgoing = {'Scenario':i, 'Region':'DE', 'Parameter':'Transmission_Outgoing', 'Unit':'GWh',
-                              'Value':total_outgoing}
-            df_total_incoming = plot_data[(plot_data.loc[:, 'Parameter'] =='Transmission_Flows_Electricity_Grid') &
-                                          (plot_data.loc[:, 'Scenario'] == i) &
-                                          (plot_data.loc[:, 'Region'].str.contains('DE_'))]
-            total_incoming = df_total_incoming['Value'].sum()
-            row_total_ingoing = {'Scenario': i, 'Region': 'DE', 'Parameter': 'Transmission_Incoming', 'Unit': 'GWh',
-                                  'Value': total_incoming}
-            df_total_losses = plot_data[(plot_data.loc[:, 'Parameter'] =='Transmission_Losses_Electricity_Grid') &
-                                          (plot_data.loc[:, 'Scenario'] == i)]
-            total_losses = -df_total_losses['Value'].sum()
-            row_total_losses = {'Scenario':i, 'Region':'DE', 'Parameter':'Transmission_Losses', 'Unit':'GWh',
-                              'Value':total_losses}
-
-            plot_data.drop(df_total_outgoing.index.to_list(), inplace=True)
-            plot_data.drop(df_total_incoming.index.to_list(),  inplace=True)
-            plot_data.drop(df_total_losses.index.to_list(), inplace=True)
-            plot_data = plot_data.append(row_total_outgoing, ignore_index=True)
-            plot_data = plot_data.append(row_total_ingoing, ignore_index=True)
-            plot_data = plot_data.append(row_total_losses, ignore_index=True)
+        for scenario in ('FlexMex2_1a', 'FlexMex2_1b', 'FlexMex2_1c', 'FlexMex2_1d'):
+            plot_data = sum_transmissions(plot_data, scenario, 'DE')
+    elif onxaxes == 'Region':
+        for region in ('AT', 'BE', 'CH', 'CZ', 'DE', 'DK', 'FR', 'IT', 'LU', 'NL', 'PL'):
+            plot_data = sum_transmissions(plot_data, 'FlexMex2_1c', region)
+            import pdb
+            pdb.set_trace
 
     df_plot_conversion_electricity = pd.crosstab(index=plot_data[onxaxes], columns=plot_data.Parameter,
                                            values=plot_data.Value, aggfunc='mean')
@@ -59,3 +74,39 @@ def conversion_electricity_FlexMex2_1(plot_data, onxaxes):
                                                             'Transmission_Outgoing'])
     return df_plot_conversion_electricity
 
+def conversion_electricity_FlexMex2_2(plot_data, onxaxes):
+    plot_data = onxaxes_preparation(plot_data, onxaxes, 'FlexMex2_2c')
+    plot_data.to_csv('2021-07-03_plot_data.csv')
+    import pdb
+    pdb.set_trace()
+    parameters = ['EnergyConversion_SecondaryEnergy_Electricity_CH4_GT',
+                  'EnergyConversion_SecondaryEnergy_Electricity_ElectricityHeat_CH4_ExCCGT',
+                  'EnergyConversion_SecondaryEnergy_Electricity_RE',
+                  'EnergyConversion_SecondaryEnergy_Electricity_Hydro_ReservoirTurbine',
+                  'Transport_FeedIn_DrivePower_Electricity',
+                  'EnergyConversion_SecondaryEnergy_Electricity_Slack',
+                  'EnergyConversion_Curtailment_Electricity_RE',
+                  'Transmission_Flows_Electricity_Grid',
+                  'Transmission_Losses_Electricity_Grid']
+    plot_data = plot_data.loc[plot_data['Parameter'].isin(parameters)]
+    # sum all outgoing and all ingoing transmissions for each scenario
+    if onxaxes == 'Scenario':
+        for scenario in ('FlexMex2_2a', 'FlexMex2_2b', 'FlexMex2_2c', 'FlexMex2_2d'):
+            plot_data = sum_transmissions(plot_data, scenario, 'DE')
+    elif onxaxes == 'Region':
+        for region in ('AT', 'BE', 'CH', 'CZ', 'DE', 'DK', 'FR', 'IT', 'LU', 'NL', 'PL'):
+            plot_data = sum_transmissions(plot_data, 'FlexMex2_2c', region)
+        import pdb
+        pdb.set_trace
+
+    df_plot_conversion_electricity = pd.crosstab(index=plot_data[onxaxes], columns=plot_data.Parameter,
+                                           values=plot_data.Value, aggfunc='mean')
+#    df_plot_conversion_electricity = \
+#            df_plot_conversion_electricity.reindex(columns=['EnergyConversion_SecondaryEnergy_Electricity_CH4_GT',
+#                                                            'EnergyConversion_SecondaryEnergy_Electricity_RE',
+#                                                            'Transmission_Incoming',
+#                                                            'EnergyConversion_SecondaryEnergy_Electricity_Slack',
+#                                                            'EnergyConversion_Curtailment_Electricity_RE',
+#                                                            'Transmission_Losses',
+#                                                            'Transmission_Outgoing'])
+    return df_plot_conversion_electricity
--- scripts/energy_flows_DE.py
+++ scripts/energy_flows_DE.py
@@ -9,45 +9,34 @@ import os
 
 from analysis.plot import preprocessing_stacked_scalars
 from analysis.plot import stacked_scalars
+from analysis.preprocessing_scalars import conversion_electricity_FlexMex2_1
+from analysis.preprocessing_scalars import conversion_electricity_FlexMex2_2
 
-# The input file must be a scalars file from oemof-flexmex, e.g. oemof-flexmex/results/FlexMex2/Scalars.csv
-# (for models 2_1) or oemof-flexmex/results/FlexMex2_2a/Scalars.csv for the model 2_2a.
+scalars_file = os.path.join(os.path.dirname(__file__), '../../oemof-flexmex/results/FlexMex2/Scalars.csv')
+scenario = sys.argv[1] # either FlexMex2_1 or FlexMex2_2
+onxaxes = sys.argv[2] # either Region or Scenario
 
+df_in = pd.read_csv(scalars_file)
+df_in.rename(columns = {'UseCase':'Scenario'}, inplace = True)
+df_in = df_in[df_in.loc[:,'Scenario'].str.contains(scenario)]
+if scenario == 'FlexMex2_1':
+    df_plot_conversion_electricity = conversion_electricity_FlexMex2_1(df_in, onxaxes)
+elif scenario == 'FlexMex2_2':
+    df_plot_conversion_electricity = conversion_electricity_FlexMex2_2(df_in, onxaxes)
 
-input_file_definition = sys.argv[1]
-
-df_in = pd.read_csv(input_file_definition)
-df_in = df_in[df_in.loc[:,'UseCase'].str.contains('FlexMex2_2')]
 # Retrieve the demand; demand is the same in all scenarios.
 # TODO: This is true for demand, but is it also for the other values imported from this scalars file?
 demand_file = os.path.join(os.path.dirname(__file__),
                               '../../oemof-flexmex/data/In/v0.09/FlexMex2_Scalars_2a.csv')
 
 df_demand = pd.read_csv(demand_file)
-df_demand.rename(columns = {'Scenario':'UseCase'}, inplace = True)
 
 # Remove Electricity_HeatPump, because HetPumps are explicitly modelled.
 df_demand = df_demand[~ (df_demand.loc[:, 'Parameter'] == 'Energy_FinalEnergy_Electricity_HeatPump')]
 # Remove FinalEnergy_H2, because I only regard Heat and Electricity.
 df_demand = df_demand[~ (df_demand.loc[:, 'Parameter'] == 'Energy_FinalEnergy_H2')]
 df_demand = df_demand[df_demand.loc[:, 'Parameter'].str.contains('Energy_FinalEnergy|Transport_AnnualDemand_Electricity_Cars')]
-df_in = df_in.append(df_demand)
+# df_in = df_in.append(df_demand)
+# df_plot_conversion_electricity = df_plot_conversion_electricity.append(df_demand)
 
-# TODO: the two lines below should be input variables to the function
-df_energy = df_in.loc[df_in['Unit'].str.contains('GWh|GWh/a'), :]
-#df_energy_DE = df_energy.loc[df_energy['Region'] == 'DE', :]
-
-df_energy_DE = df_energy.loc[df_energy['Region'].str.contains('DE'), :]
-conversion_heat, conversion_electricity, storage_heat, storage_electricity, capacity_electricity, capacity_heat \
-    = preprocessing_stacked_scalars(df_energy_DE, 1, 'UseCase')
-#for df in [df_plot_conversion_heat, df_plot_conversion_electricity, df_plot_storage_heat, df_plot_storage_electricity]:
-#    print("What should be the plot's title? e.g. 'Energy flows in Germany for FlexMex2_1'. "
-#          "The dataframe contains the following parameters ", df.columns)
-#    title = input()
-#    stacked_scalars(df, title, 'Energy in GWh', 'Scenario')
-print("Which scenario is this? FlexMex2_1 or FlexMex2_2?")
-scenario = input()
-stacked_scalars(conversion_heat, 'Heat flows in Germany in ' + scenario, 'Energy in GWh', 'Scenario')
-stacked_scalars(conversion_electricity, 'Electricity flows in Germany in ' + scenario, 'Energy in GWh', 'Scenario')
-stacked_scalars(storage_heat, 'Heat flows in Germany in ' + scenario, 'Energy in GWh', 'Scenario')
-stacked_scalars(storage_electricity, 'Electricity flows in Germany in ' + scenario, 'Energy in GWh', 'Scenario')
+stacked_scalars(df_plot_conversion_electricity, '2021-07-03-electricity_flows_FlexMex2_1c_all_countries', 'electricity in GWh', 'Scenario')
