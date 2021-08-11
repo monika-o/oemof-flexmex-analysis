5f9b291ad68ff00eba617d3d0658e9adb76a381c
monika <monika@orl25.de>
2021-08-11 09:41:36 +0200

    expand number of possible storages for storage level plotting

--- scripts/run_dispatch_plots.py
+++ scripts/run_dispatch_plots.py
@@ -3,16 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import analysis.dispatch_plots as plots
 from analysis.plot import make_colors_odict
+from matplotlib import gridspec
 
 colors_odict = make_colors_odict()
 
-input_file = os.path.join(os.path.dirname(__file__), '../../oemof-flexmex/results/FlexMex2_107/FlexMex2_1b/'
+input_file = os.path.join(os.path.dirname(__file__), '../../oemof-flexmex/results/FlexMex2_107/FlexMex2_2d/'
                                                      'oemoflex-timeseries/DE-electricity.csv'
                           )
 data = pd.read_csv(input_file, header=[0, 1, 2], parse_dates=[0], index_col=[0])
 
 fig, ax = plt.subplots(figsize=(12,5))
-ax1 = plt.subplot(2, 1, 1)
+gs = gridspec.GridSpec(ncols=1, nrows=3, figure=fig)
+
+ax1 = plt.subplot(gs[0:2, :])
+#ax1 = plt.subplot(2, 1, 1)
 data = plots.eng_format(ax1, data, "W", 1000)
 start_date='2050-01-01 00:00:00'
 end_date='2050-12-01 00:00:00'
@@ -23,8 +27,8 @@ plots.plot_dispatch(
 
 datetimeindex = pd.date_range(start="2019-01-01", periods=8760, freq="H")
 df = pd.DataFrame()
-technologies = ['LiIonBattery']
-scenario = '1b'
+technologies = ['LiIonBattery', 'H2Cavern']
+scenario = '2d'
 for technology in technologies:
     # choose 'Heat' or 'Electricity' manually
     input_file = os.path.join(os.path.dirname(__file__), '../../oemof-flexmex/results/FlexMex2_107/FlexMex2_'+scenario+'/Storage/'
@@ -35,11 +39,14 @@ for technology in technologies:
     df = df / 1000 # conversion from MWh to GWh
 df.set_index(datetimeindex, inplace=True)
 
-ax2 = plt.subplot(2, 1, 2, sharex = ax1)
-ax2.plot(df, label = df.columns.values[0], color=colors_odict[technology])
+ax2 = plt.subplot(gs[2, :], sharex=ax1)
+#ax2 = plt.subplot(2, 1, 2, sharex=ax1)
+for i in df.columns:
+    ax2.plot(df.index, df[i], color=colors_odict[i])
+#plt.set_color(colors_odict)
 ax2.set_ylabel('Storage level in GWh')
-plt.legend()
 
-plt.legend(loc="best")
+plt.legend(df.columns, loc="best")
+
 plt.tight_layout()
 plt.show()
