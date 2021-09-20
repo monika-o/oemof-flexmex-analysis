fc808e53bd47b7ff65e238822901af88de305345
monika <monika@orl25.de>
2021-09-20 14:34:20 +0200

    rename co-generation turbine into extraction CHP

--- scripts/exccgt_timeseries.py
+++ scripts/exccgt_timeseries.py
@@ -17,16 +17,16 @@ input_CCGT = os.path.join(os.path.dirname(__file__), '../../oemof-flexmex/result
 helper_df = pd.read_csv(input_CCGT)
 helper_df = helper_df/1000 # conversion from MW to GW
 
-df['Co-generation turbine'] = helper_df.iloc[:, 1]
+df['Extraction CHP'] = helper_df.iloc[:, 1]
 
 input_H2 = os.path.join(os.path.dirname(__file__), '../../oemof-flexmex/results/FlexMex2_107/FlexMex2_'+scenario+
                               '/Storage/Electricity/'+parameter_storage+'/Level/FlexMex2_'+scenario+'_oemof_DE_2050.csv')
 helper_df = pd.read_csv(input_H2)
 helper_df = helper_df/1000000 #conversion from MWh to TWh
 
-df[parameter_storage] = helper_df.iloc[:, 1]
+df['H2 cavern'] = helper_df.iloc[:, 1]
 
 
 
-plot(df, 'Co-generation turbine and H2 cavern storage levels in FlexMex2_'+scenario,
+plot(df, 'Extraction CHP turbine and H2 cavern storage levels in FlexMex2_'+scenario,
      'GW (turbine) or TWh (cavern)', start_date=start_date, end_date=end_date)
